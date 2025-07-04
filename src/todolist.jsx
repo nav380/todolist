8532d00aaab768f0e5801dec8bb82b71154f64fb
Naveen chauhan <143591216+nav380@users.noreply.github.com>
2025-07-04 20:49:52 +0530

    Update todolist.jsx

--- src/todolist.jsx
+++ src/todolist.jsx
@@ -5,7 +5,6 @@ function Todolist() {
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState('');
   const [newError, setError] = useState('');
-  const [newLoading, setLoading] = useState('');
   
 
   

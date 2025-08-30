1b536149d69036421701dca30bd4274c561d66b9
Naveen chauhan <143591216+nav380@users.noreply.github.com>
2025-08-30 22:30:21 +0530

    Update todolist.jsx

--- src/todolist.jsx
+++ src/todolist.jsx
@@ -8,7 +8,7 @@ function Todolist() {
 
 const [newLoading, setLoading] = useState('');
 
-
+const [newLoading, setLoading] = useState('');
   const handleAddTodo = () => {
     if (newTodo.trim()) {
       setTodos([...todos, newTodo]);
